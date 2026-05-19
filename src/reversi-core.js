d495a23f1db5ed6627b8b1af7e819af129c29434
utunomiya30-design <utunomiya30@gmail.com>
2026-05-19 23:53:37 +0900

    Prevent coordinated early elimination in 4-player starts

--- src/reversi-core.js
+++ src/reversi-core.js
@@ -61,10 +61,10 @@ function getInitialPositions(size, playerCount) {
   }
 
   return mapSeedPattern(size, [
-    [1, 4, 3, 2],
-    [4, 1, 3, 3],
-    [4, 3, 4, 1],
-    [2, 2, 1, 2],
+    [3, 2, 4, 3],
+    [4, 2, 1, 2],
+    [2, 1, 3, 4],
+    [1, 3, 4, 1],
   ]);
 }
 
